4f4cf2a55b403acc7b50c4b24eff78712075cfc7
danielaRen21 <daniela.f.renteria@gmail.com>
2026-01-09 11:49:08 -0600

    Rename TipPercentageForm to TipForm to fix casing issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MenuItem from './components/MenuItem';
 import useOrder from './hook/useOrder';
 import OrderContents from './components/OrderContents';
 import OrderTotal from './components/OrderTotal';
-import TipPercentageForm from './components/TipPercentageForm';
+import TipPercentageForm from './components/TipForm';
 
 function App() {
   const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder();
